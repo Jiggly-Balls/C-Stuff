0aaf23a421222ecad49b8a82815b4ef1b477dfe1
Jiggly Balls <jigglyballs9000@gmail.com>
2025-02-09 01:20:57 +0530

    Minor changes

--- learning/pointers.c
+++ learning/pointers.c
@@ -1,3 +1,8 @@
+/*
+A simple task to create a "Person" struct while creating an 
+array of it and define every persons attributes from the user
+*/
+
 #include <stdio.h>
 
 
@@ -15,9 +20,9 @@ int main() {
 
     for (int i = 0; i < persons_size; ++i) {
         printf("\nEnter the name of the number %d person: ", i);
-        scanf("%s", &(p_person -> name));
+        scanf("%s", &(p_person->name));
         printf("Enter the age of the number %d person: ", i);
-        scanf("%d", &(p_person -> age));
+        scanf("%d", &(p_person->age));
 
         ++p_person;
     }
@@ -25,8 +30,8 @@ int main() {
     p_person = p_person_begin;
 
     for (int i = 0; i < persons_size; ++i) {
-        printf("\n\nName of number %d person is: %s", i, p_person -> name);
-        printf("\nAge of the number %d person is: %d", i, p_person -> age);
+        printf("\n\nName of number %d person is: %s", i, p_person->name);
+        printf("\nAge of the number %d person is: %d", i, p_person->age);
 
         ++p_person;
     }
